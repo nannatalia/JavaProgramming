3c516e183feb63ea8ed7cb267fcde0eddfa85918
Nan Natalia <nan_natalia@yahoo.ro>
2023-01-06 09:56:12 +0200

    Commit Day 45

--- src/day45_Abstraction/PropertiesOfInterface.java
+++ src/day45_Abstraction/PropertiesOfInterface.java
@@ -1,5 +1,21 @@
 package day45_Abstraction;
 
 public interface PropertiesOfInterface {
+//    int a;
+//    static int b;
+//
+//    public PropertiesOfInterface(int a){
+//        this.a=a;
+//    }
+//    static {
+//        b=100;
+//}
+//        public void method1(){
+//            System.out.println("Instance method");
+//        }
 
+        public static void method2(){
+            System.out.println("Static method");
+        }
+        public abstract void method3();
 }
