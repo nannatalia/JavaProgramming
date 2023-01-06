2def27dfda4dc041a804cf48289ea00404e23b19
Nan Natalia <nan_natalia@yahoo.ro>
2023-01-06 09:51:53 +0200

    Commit Day 45

---
+++ src/day45_Abstraction/Shape/Shape.java
@@ -0,0 +1,23 @@
+package day45_Abstraction.Shape;
+
+public abstract class Shape {
+    private final String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract double area();
+    public abstract double perimeter();
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
