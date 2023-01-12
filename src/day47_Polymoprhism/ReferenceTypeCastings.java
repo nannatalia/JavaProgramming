9423171dc50202e766aa498c613b9dff7d5f43a5
Nan Natalia <nan_natalia@yahoo.ro>
2023-01-12 11:02:27 +0200

    Commit Day 47

--- src/day47_Polymoprhism/ReferenceTypeCastings.java
+++ src/day47_Polymoprhism/ReferenceTypeCastings.java
@@ -1,7 +1,12 @@
 package day47_Polymoprhism;
+import day43_Abstraction.employeeTask.*;
+import day44_Abstraction.Animal.*;
+import day45_Abstraction.Shape.*;
 
 public class ReferenceTypeCastings {
     public static void main(String[] args) {
 
+        Shape shape1 =(Shape) new Circle(5); //UPCASTING
+      //Shape shape = new Circle(3);
     }
 }
