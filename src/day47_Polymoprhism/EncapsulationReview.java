b500221729d00dcc0fd9df35fe99ae4c476d7c47
Nan Natalia <nan_natalia@yahoo.ro>
2023-01-18 09:25:02 +0200

    Commit Day 47

--- src/day47_Polymoprhism/EncapsulationReview.java
+++ src/day47_Polymoprhism/EncapsulationReview.java
@@ -1,4 +1,30 @@
 package day47_Polymoprhism;
 
 public class EncapsulationReview {
+
+    private String bookName;
+    private final String publishDate;
+
+    public EncapsulationReview(String bookName, String publishDate) {
+        setBookName(bookName);
+        this.publishDate = publishDate;
+    }
+
+    public String getBookName() {
+        if(bookName == null){
+            return "";
+        }
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        if(bookName == null || bookName.isEmpty()){
+            throw new RuntimeException("Invalid Data");
+        }
+        this.bookName = bookName;
+    }
+
+    public String getPublishDate() {
+        return publishDate;
+    }
 }
