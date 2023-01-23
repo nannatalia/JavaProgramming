be107ce77ac222d5c558bbd19c54f3f51b459b15
Nan Natalia <nan_natalia@yahoo.ro>
2023-01-23 10:37:27 +0200

    Commit Day 50

---
+++ src/day50_Collections/ListSetPractice.java
@@ -0,0 +1,38 @@
+package day50_Collections;
+
+import java.util.*;
+
+public class ListSetPractice {
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+        set.addAll(Arrays.asList(1,2,3,4,5,1,2,3,4,5));
+
+        System.out.println("set = " + set);
+
+        //list over array: many methods, size is dynamic
+
+        //Integer[] integerArr = set.toArray(new Integer[0]);
+
+        List<Integer> list =  new ArrayList<>(set);
+
+        System.out.println("list = " + list);
+
+//        List<String> names = null;
+//        System.out.println("names.size() = " + names.size());
+
+        System.out.println("----------------------------------------");
+
+        //pop():
+
+        List<Character> chars = new Stack<>();
+        chars.addAll(Arrays.asList('A','B','C','D'));
+
+        System.out.println("chars = " + chars);
+
+        ((Stack) chars).pop();
+
+        System.out.println("After popping: ");
+
+        System.out.println("chars = " + chars);
+    }
+}
